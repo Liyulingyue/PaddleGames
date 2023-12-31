2185df46671d0d25e3af18be8272da65952a2730
Liyulingyue <852433440@qq.com>
2023-12-31 22:24:07 +0800

    rename main

--- ErnieGames/WordsFights/main_client.py
+++ ErnieGames/WordsFights/main_client.py
@@ -13,93 +13,58 @@ import json
 class MyWindow(QWidget):
     def __init__(self):
         super().__init__()
+        self.initVariable()
+        self.initUI() # 初始化UI后不需要给MainPage赋值,如果需要赋值，则调用draw
+        self.initClock()
+
+        # 启动线程
+        self.client.start()
+
+    def initVariable(self):
         self.elements = {}
         self.width = 1600
         self.height = 900
-        self.gameobj = FightObject.get_default_dict()
-        self.initUI()
-        self.draw_frame()
-        self.client = Client()
-        self.client.start()
-        self.initClock()
+        self.game_dict = FightObject.get_default_dict()
         self.user = "User1"
+        self.client = Client()
+        self.layout_dict = self.get_layout_configure()
 
     def initUI(self):
-        Img_obj = {  # x, y, w, h
-            "User1_Img_User": ['./Source/Images/User1.png', 0.125, 0.25, 0.22, 0.22],
-            "User1_Img_Soldier": ['./Source/Images/Soldier1.png', 0.0625, 0.49, 0.073, 0.073],
-            "User1_Img_Rider": ['./Source/Images/Rider1.png', 0.0625, 0.59, 0.073, 0.073],
-            "User1_Img_Archer": ['./Source/Images/Archer1.png', 0.0625, 0.69, 0.073, 0.073],
-            "User1_Img_Choosed": ['./Source/Images/Background.png', 0.0625, 0.79, 0.073, 0.073],
-            "User2_Img_User": ['./Source/Images/User2.png', 0.875, 0.25, 0.22, 0.22],
-            "User2_Img_Soldier": ['./Source/Images/Soldier2.png', 0.825, 0.49, 0.073, 0.073],
-            "User2_Img_Rider": ['./Source/Images/Rider2.png', 0.825, 0.59, 0.073, 0.073],
-            "User2_Img_Archer": ['./Source/Images/Archer2.png', 0.825, 0.69, 0.073, 0.073],
-            "User2_Img_Choosed": ['./Source/Images/Background.png', 0.825, 0.79, 0.073, 0.073],
-            "Img_Main": ['./Source/Images/Background.png', 0.5, 0.5, 0.98, 0.98],
-        }
-        self.Img_obj = Img_obj
-        Text_obj = {
-            "User1_Text_User": ['用户1', 0.125, 0.4, 0.18, 0.067],
-            "User1_Text_Soldier": ['士兵', 0.1375, 0.49, 0.18, 0.073],
-            "User1_Text_Rider": ['骑兵', 0.1375, 0.59, 0.18, 0.073],
-            "User1_Text_Archer": ['弓兵', 0.1375, 0.69, 0.18, 0.073],
-            "User1_Text_Choosed": ['未选择', 0.1375, 0.79, 0.18, 0.073],
-            "User2_Text_User": ['用户2', 0.875, 0.4, 0.18, 0.067],
-            "User2_Text_Soldier": ['士兵', 0.9125, 0.49, 0.18, 0.073],
-            "User2_Text_Rider": ['骑兵', 0.9125, 0.59, 0.18, 0.073],
-            "User2_Text_Archer": ['弓兵', 0.9125, 0.69, 0.18, 0.073],
-            "User2_Text_Choosed": ['未选择', 0.9125, 0.79, 0.18, 0.073]
-        }
-        Line_obj = {
-            "User1_Line_Input": ["", 0.08, 0.89, 0.18, 0.073],
-            "User2_Line_Input": ["", 0.85, 0.89, 0.18, 0.073],
-        }
-        Button_obj = {
-            "User1_Button_Confirm": ["发送", 0.18, 0.89, 0.073, 0.073],
-            "User2_Button_Confirm": ["发送", 0.95, 0.89, 0.073, 0.073],
-        }
-
-        for key in Img_obj:
-            self.elements[key] = QLabel(self)
-            fig_path, x, y, w, h = Img_obj[key]
+        for key in self.layout_dict:
+            user, type, name = key.split("_")
+            cfg_str, x, y, w, h = self.layout_dict[key]
             w = int(w * self.height) if w < 1 else w
             h = int(h * self.height) if h < 1 else h
             x = int(x * self.width - w / 2) if x < 1 else x
             y = int(y * self.height - h / 2) if y < 1 else y
-            pixmap = QPixmap(fig_path)  # 加载本地图片
-            pixmap = pixmap.scaled(w, h)
-            self.elements[key].setPixmap(pixmap)  # 将图片设置到QLabel中
-            # self.elements[key].setFixedSize(w, h)
-            # self.elements[key].move(x, y)
-            self.elements[key].setGeometry(x, y, w, h)  # 设置标签的位置和大小
-        for key in Text_obj:
-            text, x, y, w, h = Text_obj[key]
-            w = int(w * self.height) if w < 1 else w
-            h = int(h * self.height) if h < 1 else h
-            x = int(x * self.width - w / 2) if x < 1 else x
-            y = int(y * self.height - h / 2) if y < 1 else y
-            self.elements[key] = QLabel(self)
-            self.elements[key].setText(text)
-            # self.elements[key].move(x, y)
-            self.elements[key].setGeometry(x, y, w, h)  # 设置标签的位置和大小
-        for key in Line_obj:
-            text, x, y, w, h = Line_obj[key]
-            w = int(w * self.height) if w < 1 else w
-            h = int(h * self.height) if h < 1 else h
-            x = int(x * self.width - w / 2) if x < 1 else x
-            y = int(y * self.height - h / 2) if y < 1 else y
-            self.elements[key] = QLineEdit(self)
-            self.elements[key].move(x, y)
-        for key in Button_obj:
-            text, x, y, w, h = Button_obj[key]
-            w = int(w * self.height) if w < 1 else w
-            h = int(h * self.height) if h < 1 else h
-            x = int(x * self.width - w / 2) if x < 1 else x
-            y = int(y * self.height - h / 2) if y < 1 else y
-            self.elements[key] = QPushButton(text, self)
-            self.elements[key].move(x, y)
-            self.elements[key].clicked.connect(self.send_prompt)
+            if type == "Img":
+                fig_path = cfg_str
+                self.elements[key] = QLabel(self)
+                pixmap = QPixmap(fig_path)  # 加载本地图片
+                pixmap = pixmap.scaled(w, h)
+                self.elements[key].setPixmap(pixmap)  # 将图片设置到QLabel中
+                # self.elements[key].setFixedSize(w, h)
+                # self.elements[key].move(x, y)
+                self.elements[key].setGeometry(x, y, w, h)  # 设置标签的位置和大小
+            elif type == "Text":
+                text = cfg_str
+                self.elements[key] = QLabel(self)
+                self.elements[key].setText(text)
+                # self.elements[key].move(x, y)
+                self.elements[key].setGeometry(x, y, w, h)  # 设置标签的位置和大小
+            elif type == "Line":
+                text = cfg_str
+                self.elements[key] = QLineEdit(self)
+                self.elements[key].move(x, y)
+            elif type == "Button":
+                text = cfg_str
+                self.elements[key] = QPushButton(text, self)
+                self.elements[key].move(x, y)
+                self.elements[key].clicked.connect(self.send_prompt)
+            else:
+                print(f"Unknown key: {key}, "
+                      f"the key must be in format `USER_TYPE_NAME`, "
+                      f"please check the configure function.")
 
         # 设置窗口的位置和大小
         self.setGeometry(0, 0, self.width, self.height)
@@ -121,19 +86,19 @@ class MyWindow(QWidget):
         if event.button() == Qt.LeftButton:
             for key in ["Soldier", "Rider", "Archer"]:
                 flag = 0
-                hp, x_percent, y_percent, _, _, _ = self.gameobj[self.user][key]
+                hp, x_percent, y_percent, _, _, _ = self.game_dict[self.user][key]
                 if hp <= 0: continue
-                _, _x, _y, _w, _h = self.Img_obj[f"{self.user}_Img_{key}"]
+                _, _x, _y, _w, _h = self.layout_dict[f"{self.user}_Img_{key}"]
                 if _x - _w * self.height / self.width / 2 < x < _x + _w * self.height / self.width / 2 and _y - _h / 2 < y < _y + _h / 2:
                     flag = 1
-                _, __x, __y, __w, __h = self.Img_obj[f"Img_Main"]
+                _, __x, __y, __w, __h = self.layout_dict[f"_Img_Main"]
                 _x = __x - __w * self.height / self.width / 2 + __w * self.height / self.width * x_percent
                 _y = __y - __h / 2 + __h * y_percent
                 if _x - _w * self.height / self.width / 2 < x < _x + _w * self.height / self.width / 2 and _y - _h / 2 < y < _y + _h / 2:
                     flag = 1
                 if flag == 1:
-                    self.Img_obj[f"{self.user}_Img_Choosed"][0] = f'./Source/Images/{key}1.png'
-                    fig_path, x, y, w, h = self.Img_obj[f"{self.user}_Img_Choosed"]
+                    self.layout_dict[f"{self.user}_Img_Choosed"][0] = f'./Source/Images/{key}1.png'
+                    fig_path, x, y, w, h = self.layout_dict[f"{self.user}_Img_Choosed"]
                     w = int(w * self.height) if w < 1 else w
                     h = int(h * self.height) if h < 1 else h
                     x = int(x * self.width - w / 2) if x < 1 else x
@@ -143,7 +108,7 @@ class MyWindow(QWidget):
                     self.elements[f"{self.user}_Img_Choosed"].setPixmap(pixmap)  # 将图片设置到QLabel中
                     self.elements[f"{self.user}_Text_Choosed"].setText(f"{key}")
         elif event.button() == Qt.RightButton:
-            _, _x, _y, _w, _h = self.Img_obj[f"Img_Main"]
+            _, _x, _y, _w, _h = self.layout_dict[f"_Img_Main"]
             _x_real = _x * self.width - _w / 2 * self.height
             _y_real = _y * self.height - _h / 2 * self.height
             x_based = x_real - _x_real
@@ -157,8 +122,8 @@ class MyWindow(QWidget):
                 key = self.elements[f"{self.user}_Text_Choosed"].text()
                 if key in ["Soldier", "Rider", "Archer"]:
                     self.client.set_send_str(f"MOVE,{self.user},{key},{str(new_x_p)},{str(new_y_p)}")
-                    # self.gameobj["User1"][key][1] = new_x_p
-                    # self.gameobj["User1"][key][2] = new_y_p
+                    # self.game_dict["User1"][key][1] = new_x_p
+                    # self.game_dict["User1"][key][2] = new_y_p
                     # self.draw_frame()
         else:
             pass
@@ -166,35 +131,64 @@ class MyWindow(QWidget):
     def update_frame(self):
         raw_str = self.client.get_received_str()
         if raw_str != "":
-            self.gameobj = json.loads(raw_str)
+            self.game_dict = json.loads(raw_str)
             self.draw_frame()
 
     def draw_frame(self):
-        img = get_img_and_resize(self.Img_obj, "Img_Main")
-        for key in self.gameobj["User1"]:
-            hp, x_percent, y_percent, _, _, prompt = self.gameobj["User1"][key]
+        img = get_img_and_resize(self.layout_dict, "_Img_Main")
+        for key in self.game_dict["User1"]:
+            hp, x_percent, y_percent, _, _, prompt = self.game_dict["User1"][key]
             self.elements[f"User1_Text_{key}"].setText(f"血量: {str(hp)}\n阵法: {prompt}")
             if hp <= 0: continue
-            part_img = get_img_and_resize(self.Img_obj, f"User1_Img_{key}")
+            part_img = get_img_and_resize(self.layout_dict, f"User1_Img_{key}")
             img = paste_image(img, part_img, [x_percent, y_percent])
-        for key in self.gameobj["User2"]:
-            hp, x_percent, y_percent, _, _, prompt = self.gameobj["User2"][key]
+        for key in self.game_dict["User2"]:
+            hp, x_percent, y_percent, _, _, prompt = self.game_dict["User2"][key]
             self.elements[f"User2_Text_{key}"].setText(f"血量: {str(hp)}\n阵法: {prompt}")
             if hp <= 0: continue
-            part_img = get_img_and_resize(self.Img_obj, f"User2_Img_{key}")
+            part_img = get_img_and_resize(self.layout_dict, f"User2_Img_{key}")
             img = paste_image(img, part_img, [x_percent, y_percent])
         image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         height, width, channel = image.shape
         bytesPerLine = 3 * width
         qtImage = QImage(image.data, width, height, bytesPerLine, QImage.Format_RGB888)
         pixmap = QPixmap.fromImage(qtImage)
-        self.elements["Img_Main"].setPixmap(pixmap)  # 将图片设置到QLabel中
+        self.elements["_Img_Main"].setPixmap(pixmap)  # 将图片设置到QLabel中
 
     def send_prompt(self):
         prompt = self.elements[f"{self.user}_Line_Input"].text()
         key = self.elements[f"{self.user}_Text_Choosed"].text()
         self.client.set_send_str(f"ERNIE,{self.user},{key},{prompt}")
 
+    def get_layout_configure(self):
+        layout_dict = { # x, y, w, h
+            "User1_Img_User": ['./Source/Images/User1.png', 0.125, 0.25, 0.22, 0.22],
+            "User1_Img_Soldier": ['./Source/Images/Soldier1.png', 0.0625, 0.49, 0.073, 0.073],
+            "User1_Img_Rider": ['./Source/Images/Rider1.png', 0.0625, 0.59, 0.073, 0.073],
+            "User1_Img_Archer": ['./Source/Images/Archer1.png', 0.0625, 0.69, 0.073, 0.073],
+            "User1_Img_Choosed": ['./Source/Images/Background.png', 0.0625, 0.79, 0.073, 0.073],
+            "User2_Img_User": ['./Source/Images/User2.png', 0.875, 0.25, 0.22, 0.22],
+            "User2_Img_Soldier": ['./Source/Images/Soldier2.png', 0.825, 0.49, 0.073, 0.073],
+            "User2_Img_Rider": ['./Source/Images/Rider2.png', 0.825, 0.59, 0.073, 0.073],
+            "User2_Img_Archer": ['./Source/Images/Archer2.png', 0.825, 0.69, 0.073, 0.073],
+            "User2_Img_Choosed": ['./Source/Images/Background.png', 0.825, 0.79, 0.073, 0.073],
+            "_Img_Main": ['./Source/Images/Background.png', 0.5, 0.5, 0.98, 0.98],
+            "User1_Text_User": ['用户1', 0.125, 0.4, 0.18, 0.067],
+            "User1_Text_Soldier": ['士兵', 0.1375, 0.49, 0.18, 0.073],
+            "User1_Text_Rider": ['骑兵', 0.1375, 0.59, 0.18, 0.073],
+            "User1_Text_Archer": ['弓兵', 0.1375, 0.69, 0.18, 0.073],
+            "User1_Text_Choosed": ['未选择', 0.1375, 0.79, 0.18, 0.073],
+            "User2_Text_User": ['用户2', 0.875, 0.4, 0.18, 0.067],
+            "User2_Text_Soldier": ['士兵', 0.9125, 0.49, 0.18, 0.073],
+            "User2_Text_Rider": ['骑兵', 0.9125, 0.59, 0.18, 0.073],
+            "User2_Text_Archer": ['弓兵', 0.9125, 0.69, 0.18, 0.073],
+            "User2_Text_Choosed": ['未选择', 0.9125, 0.79, 0.18, 0.073],
+            "User1_Line_Input": ["", 0.08, 0.89, 0.18, 0.073],
+            "User2_Line_Input": ["", 0.85, 0.89, 0.18, 0.073],
+            "User1_Button_Confirm": ["发送", 0.18, 0.89, 0.073, 0.073],
+            "User2_Button_Confirm": ["发送", 0.95, 0.89, 0.073, 0.073],
+        }
+        return layout_dict
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
